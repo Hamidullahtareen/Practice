759075b7c0ddd4e33dadd1ac4050c883de8fd0f3
hamidullahtareen <hamidullah.tareen@metropolia.fi>
2026-08-20 17:58:15 +0300

    fixed bug

--- App/Back.py
+++ App/Back.py
@@ -1,4 +1,4 @@
 number_one = 10
 number_two = 20
 sum = number_one + number_two
-print("The sum of", number_one, "and", number_two, "is:", sum
+print("The sum of", number_one, "and", number_two, "is:", sum)  
